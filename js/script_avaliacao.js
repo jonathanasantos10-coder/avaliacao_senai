d5c5778bb51b5c75172ab3988bc22c894c26dfb1
Jonathan <jonathan.a.santos10@aluno.senai.br>
2026-06-26 20:26:57 -0300

    toda a parte lógica funciona, falta, se possível, fazer uma formatação para ficar mais fácil de ler o resultado. o que eu não consegui fazer com toFixed (2), vou partir para o css agora.

--- js/script_avaliacao.js
+++ js/script_avaliacao.js
@@ -29,13 +29,13 @@ const addAp = (objAp) => {
 const listaApartamento = () => {
     divResult.innerHTML = ''
     apartamento.forEach((elem, i) => {
-    divResult.innerHTML += `<div-class = 'apNovo'> Lote ${i + 1} <br> Descrição: Quadra ${elem.quadra} - Lote ${elem.lote} <br> Largura do lote: ${elem.largura} <br> Comprimento do lote: ${elem.comprimento} <br> ${calculoArea(elem)} <br> ${calculoLote(elem)} <br> taxa do lote: ${taxalote(elem)}` // colocar to fixed para deixar legível em forma de moeda no calculolote
+    divResult.innerHTML += `<div-class = 'apNovo'> Lote ${i + 1} <br> Descrição: Quadra ${elem.quadra} - Lote ${elem.lote} <br> Largura do lote: ${elem.largura} <br> Comprimento do lote: ${elem.comprimento} <br> Área do Apartamento em M²: ${parseFloat(calculoArea(elem)).toFixed(2)} <br> Valor do lote em R$: ${parseFloat(calculoLote(elem)).toFixed(2).replace('.',',')} <br> Valor da taxa do lote: ${taxalote(elem)}` 
 })
 }
 let  aream2 = ''
 
 const calculoArea = (objAp) =>{
-    aream2 = `${parseFloat(objAp.largura) * (objAp.comprimento)}`
+    aream2 = `${(objAp.largura) * (objAp.comprimento)}`
     return aream2
 
 }
@@ -43,15 +43,20 @@ let valorLote = ''
 let valorFixo = 550
 
 const calculoLote = (calculoArea) =>{
-    valorLote = `${parseFloat(aream2) * (valorFixo)}` 
+    valorLote = `${(aream2) * (valorFixo)}` 
     return valorLote
 } 
 let taxa = ''
 const taxalote = (calculoLote) => {
     if ((valorLote) < 20000){
-    taxa = `Isento de taxa`
-    } else
-    taxa = `tudo companheiro`
-
+    taxa = `Isento de taxa pois o valor do seu lote é menor que 20 Mil reais.`
+    }else if ((valorLote) >20000 <100000){
+    taxa = `${parseFloat(valorLote * 0.05).toFixed(2).replace('.',',')}`
+    }else if((valorLote) >100000 < 500000){
+    taxa = `${parseFloat(valorLote * 0.10).toFixed(2).replace('.',',')}`
+    }else if((valorLote) >500000 <1000000){
+    taxa = `${parseFloat(valorLote * 0.15).toFixed(2).replace('.',',')}`
+    }else 
+    taxa = `${parseFloat(valorLote * 0.20).toFixed(2).replace('.',',')}`
     return taxa
 }
