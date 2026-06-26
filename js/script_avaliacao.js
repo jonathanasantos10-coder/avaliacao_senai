6d0204ea7b364f2eaa56d9c3368950b1ab210fe2
Jonathan <jonathan.a.santos10@aluno.senai.br>
2026-06-26 19:37:52 -0300

    adicionado cálculo de área do lote, tudo funcionando. adicionado input de quadra e lote no html pois não havia.

--- js/script_avaliacao.js
+++ js/script_avaliacao.js
@@ -1,15 +1,19 @@
 const apartamento = [] 
 const formAp = document.querySelector('#form-lote')
 const divResult = document.querySelector('#resultado-lote')
+const largura = document.querySelector('#largura')
+const comprimento = document.querySelector('#comprimento')
+
 
 formAp.addEventListener('submit', (evt) =>{ 
     evt.preventDefault()
-
     const dadosForm = new FormData(formAp)
-
+  
     const ap = {
+        quadra: dadosForm.get('quadra'),
+        lote: dadosForm.get('lote'),
         largura: dadosForm.get('largura'),
-        comprimento: dadosForm.get('comprimento')
+        comprimento: dadosForm.get('comprimento'),
     }
     addAp(ap)
 
@@ -25,8 +29,11 @@ const addAp = (objAp) => {
 const listaApartamento = () => {
     divResult.innerHTML = ''
     apartamento.forEach((elem, i) => {
-    divResult.innerHTML += `<div-class = 'apNovo'> Apartamento ${i + 1} <br> Largura do lote: ${elem.largura} <br> Comprimento do lote: ${elem.comprimento}`
+    divResult.innerHTML += `<div-class = 'apNovo'> Lote ${i + 1} <br> Descrição: Quadra ${elem.quadra} - Lote ${elem.lote} <br> Largura do lote: ${elem.largura} <br> Comprimento do lote: ${elem.comprimento} <br> ${calculoArea(elem)}`
 })
 }
-
-
+let  aream2 = ''
+const calculoArea = (objAp) =>{
+    aream2 = `Área m² do AP: ${parseFloat(objAp.largura) * (objAp.comprimento)}`
+    return aream2
+}
